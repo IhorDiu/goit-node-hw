f2377b7c129957411b9ee310457d24e51ba7db59
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-05-17 23:44:13 +0300

    step 2-3

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
-console.log('Hello World!!! ')
+const contactsList = require("./contacts.js");
+
+const invokeAction = async ({
+  action,
+  id,
+  name,
+  email,
+  phone,
+}) => {
+  switch (action) {
+    case "list":
+      const allContacts =
+        await contactsList.listContacts();
+      return console.log(allContacts);
+
+    case "get":
+      const oneContact =
+        await contactsList.getContactById(id);
+      return console.log(oneContact);
+
+    case "add":
+      const newContact =
+        await contactsList.addContact({
+          name,
+          email,
+          phone,
+        });
+      return console.log(newContact);
+
+    case "remove":
+      const delContact =
+        await contactsList.removeContact(id);
+      return console.log(delContact);
+
+    default:
+      console.warn(
+        "\x1B[31m Unknown action type!"
+      );
+  }
+};
+
+// invokeAction ({action: "list"})
+// invokeAction({ action: "get", id: "dffghj" });
+// invokeAction({ action: "add", name: "Maks Shepard", email: "maks.shepard@mail.com", phone: "(050) 555-55555" });
+invokeAction({
+  action: "remove",
+  id: "uXquaLwYYALwH-9OuW",
+});
