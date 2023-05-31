88b5eb8edac50cab1f1301d202b7326fcbf1405c
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-05-31 19:00:38 +0300

    add mongoose

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-const app = require('./app')
+const mongoose = require("mongoose");
+const app = require("./app");
 
-//H0N2OJJ0jwTIlASF
+// H0N2OJJ0jwTIlASF
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://IhorD:H0N2OJJ0jwTIlASF@cluster0.pvwipli.mongodb.net/db-contacts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
