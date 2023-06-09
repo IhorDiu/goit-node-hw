608b23824cefb4dbd5760213a2dbe2fb9f37997a
Ihor Diuzhev <110743288+IhorDiu@users.noreply.github.com>
2023-06-09 16:30:38 +0300

    add  jimp package and resize avatar 250 by 250

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -1,18 +1,24 @@
 const path = require("path");
 const fs = require("fs/promises");
 const { User } = require("../../models/user");
+const Jimp = require("jimp");
 
-const avatarsDir = path.join(__dirname, "../", "../", "public", "avatars");
+const avatarsDir = path.join(__dirname, "..", "..", "public", "avatars");
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
   const { path: tempUpload, originalname } = req.file;
 
-const filename = `${_id}_${originalname}`
+  const filename = `${_id}_${originalname}`;
 
   const resultUpload = path.join(avatarsDir, filename);
 
   await fs.rename(tempUpload, resultUpload);
+
+  Jimp.read(resultUpload, (err, avatar) => {
+    if (err) throw err;
+    avatar.resize(250, 250).write(resultUpload);
+  });
 
   const avatarURL = path.join("avatars", filename);
 
